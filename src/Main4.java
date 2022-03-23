154cdcdcee22f33f61a505cb47e2cc6b32f0e7a2
ваше имяAlexander <sanj12@ya.ru>
2022-03-24 00:04:48 +0500

    I've written down changes!

--- src/Main4.java
+++ src/Main4.java
@@ -2,5 +2,6 @@ public class Main4 {
     public static void main(String[] args) {
         String affirmation = VariableClass.iwont + " looser, " + VariableClass.iwill + " developer";
         System.out.println(affirmation);
+        System.out.println("Привет!!!");
     }
 }
